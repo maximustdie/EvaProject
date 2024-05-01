fcbda7274f05d5cef6547a71f606f818c4bae2c5
maximustdie <maximustdie@yandex.ru>
2024-05-02 02:59:56 +0300

    fix exceptions and added terminated process

--- daemon/cpu_load.py
+++ daemon/cpu_load.py
@@ -1,13 +1,17 @@
 import argparse
 import logging
+import signal
+import sys
+
 import requests
 
 import psutil
-from requests.exceptions import InvalidSchema
+from requests.exceptions import InvalidSchema, RequestException
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--host', type=str, default="http://127.0.0.1")
 parser.add_argument('--port', type=int, default=8001)
+parser.add_argument('--interval', type=int, default=10)
 
 args = parser.parse_args()
 
@@ -21,18 +25,27 @@ HEADERS = {
 
 def send_cpu_load(cpu_load: float):
     try:
-        response = requests.post(url=URL, headers=HEADERS, data={"load": cpu_load})
+        response = requests.post(url=URL, headers=HEADERS, json={"load": cpu_load})
+        response.raise_for_status()
         return response
-    except InvalidSchema:
-        logging.error(f"Error sending request for {URL}")
+    except RequestException as err:
+        logging.error(f"Error sending request for {URL}. Detail: {err}")
 
 
 def run():
     while True:
-        cpu_load = psutil.cpu_percent(interval=10)
-        send_cpu_load(cpu_load)
+        cpu_load = psutil.cpu_percent(interval=args.interval)
         logging.info(f"cpu load: {cpu_load}%")
+
+        send_cpu_load(cpu_load)
+
+
+def signal_handler(signum, frame):
+    logging.info("Process terminating...")
+    sys.exit(0)
 
 
 if __name__ == "__main__":
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
     run()
